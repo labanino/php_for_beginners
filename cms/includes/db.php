939629f7b0e38cacc501817098b1e39d48cdc5b5
Alex Labanino <ghostrider6610@gmail.com>
2022-06-24 21:19:29 -0400

    Making our files reusable (includes)

---
+++ cms/includes/db.php
@@ -0,0 +1,7 @@
+<?php
+
+$connection = mysqli_connect('localhost', 'root', 'root', 'cms');
+
+if($connection) {
+    echo "We are connected";
+}
---
+++ cms/includes/footer.php
@@ -0,0 +1,25 @@
+
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; www.mycms.com <?php echo date("Y");?></p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
