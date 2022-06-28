056badb45044122f0b486af9162d05f3b14cbe52
Alex Labanino <ghostrider6610@gmail.com>
2022-06-28 12:44:14 -0400

    Creating the admin category page

---
+++ cms/admin/categories.php
@@ -0,0 +1,68 @@
+<?php include "includes/header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/navigation.php"; ?>
+        
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Admin Panel
+                            <small>Username</small>
+                        </h1>
+
+                        <div class="col-xs-6">
+                        <!-- Create Categories Form -->
+                        <form action="">
+                            <div class="form-group">
+                                <label for="cat-title">Add Category</label>
+                                <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                        </form>
+                        </div>
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+<?php include "includes/footer.php"; ?> 
--- cms/admin/index.php
+++ cms/admin/index.php
@@ -1,7 +1,7 @@
 <?php include "includes/header.php"; ?>
 
-    <div id="wrapper">
-
+    <div id="wrapper"> 
+        
         <!-- Navigation -->
         <?php include "includes/navigation.php"; ?>
         
