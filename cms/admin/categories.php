5106bb70f4f051db7e6fdd2e88f410d2cb94bf26
Alex Labanino <ghostrider6610@gmail.com>
2022-06-28 13:03:07 -0400

    Displaying data in category page

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
 
     <div id="wrapper">
 
         <!-- Navigation -->
-        <?php include "includes/navigation.php"; ?>
+        <?php include "includes/admin_navigation.php"; ?>
         
         <div id="page-wrapper">
 
@@ -18,16 +18,22 @@
                         </h1>
 
                         <div class="col-xs-6">
-                        <!-- Create Categories Form -->
-                        <form action="">
-                            <div class="form-group">
-                                <label for="cat-title">Add Category</label>
-                                <input class="form-control" type="text" name="cat_title">
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-                            </div>
-                        </form>
+                            <!-- Create Categories Form -->
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="cat-title">Add Category</label>
+                                    <input class="form-control" type="text" name="cat_title">
+                                </div>
+                                <div class="form-group">
+                                    <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-xs-6">
+                            <?php
+                                $query = "SELECT * FROM categories";
+                                $select_categories = mysqli_query($connection,$query);
+                            ?>
                         </div>
                         <div class="col-xs-6">
                             <table class="table table-bordered table-striped">
@@ -38,10 +44,17 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Baseball Category</td>
-                                        <td>Basketball Category</td>
-                                    </tr>
+                                    <?php
+                                        while($row = mysqli_fetch_assoc($select_categories)) {
+                                            $cat_id = $row['cat_id'];
+                                            $cat_title = $row['cat_title'];
+
+                                            echo "<tr>";
+                                            echo "<td>{$cat_id}</td>";
+                                            echo "<td>{$cat_title}</td>";
+                                            echo "</tr>";
+                                        }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -65,4 +78,4 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
-<?php include "includes/footer.php"; ?> 
+<?php include "includes/admin_footer.php"; ?> 
