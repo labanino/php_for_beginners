61b26cfb055d476b28626b8f68fb932626a6e3f0
Alex Labanino <ghostrider6610@gmail.com>
2022-06-29 10:56:11 -0400

    Update and Edit categories

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -1,25 +1,25 @@
 <?php include "includes/admin_header.php"; ?>
 
-    <div id="wrapper">
+<div id="wrapper">
 
-        <!-- Navigation -->
-        <?php include "includes/admin_navigation.php"; ?>
-        
-        <div id="page-wrapper">
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
 
-            <div class="container-fluid">
+    <div id="page-wrapper">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Admin Panel
-                            <small>Username</small>
-                        </h1>
+        <div class="container-fluid">
 
-                        <div class="col-xs-6">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Admin Panel
+                        <small>Username</small>
+                    </h1>
 
-                            <?php
+                    <div class="col-xs-6">
+
+                        <?php
                                 // Get data after submit
                                 if(isset($_POST['submit'])) {
                                     $cat_title = $_POST['cat_title']; // Get 'cat_title' after submit 
@@ -44,28 +44,39 @@
 
                             ?>
 
-                            <!-- Create Categories Form -->
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="cat-title">Add Category</label>
-                                    <input class="form-control" type="text" name="cat_title">
-                                </div>
-                                <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-xs-6">
-                            <table class="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Category Title</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    
+                        <!-- Add Categories Form -->
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat-title">Add Category</label>
+                                <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                        </form>
+
+                        <!-- Edit Categories -->
+                        <?php
+
+                            if(isset($_GET['edit'])) {
+                                $cat_id = $_GET['edit'];
+                                include "includes/update_categories.php";
+                            }
+
+                        ?>
+
+                    </div>
+                    <div class="col-xs-6">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+
                                         // Query ID and Title from Categories table
                                         $query = "SELECT * FROM categories";
                                         $select_categories = mysqli_query($connection,$query);
@@ -78,11 +89,12 @@
                                             echo "<td>{$cat_id}</td>";
                                             echo "<td>{$cat_title}</td>";
                                             echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                            echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
                                             echo "</tr>";
                                         }
 
                                     ?>
-                                    <?php
+                                <?php
 
                                         // Delete Category Title from table
                                         if(isset($_GET['delete'])) {
@@ -94,27 +106,27 @@
                                         }
 
                                     ?>
-                                </tbody>
-                            </table>
-                        </div>
-
+                            </tbody>
+                        </table>
                     </div>
-                </div>
-                <!-- /.row -->
 
+                </div>
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
 
     </div>
-    <!-- /#wrapper -->
+    <!-- /#page-wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+</div>
+<!-- /#wrapper -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-<?php include "includes/admin_footer.php"; ?> 
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<?php include "includes/admin_footer.php"; ?>
