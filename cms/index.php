f5dc6339896a1a3454208ff1c2eab93da49ad900
Alex Labanino <ghostrider6610@gmail.com>
2022-07-03 16:04:35 -0400

    Linking title post in index to individual posts

--- cms/index.php
+++ cms/index.php
@@ -13,17 +13,18 @@
         <div class="col-md-8">
 
             <?php
-                    $query = "SELECT * FROM posts";
-                    $select_all_posts_query = mysqli_query($connection,$query);
 
-                    while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = $row['post_content'];
-                    ?>
+                $query = "SELECT * FROM posts";
+                $select_all_posts_query = mysqli_query($connection,$query);
 
+                while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                ?>
 
             <h1 class="page-header">
                 Page Heading
@@ -31,12 +32,8 @@
             </h1>
 
             <!-- First Blog Post -->
-            <h2>
-                <a href="#"><?php echo $post_title; ?></a>
-            </h2>
-            <p class="lead">
-                by <a href="index.php"><?php echo $post_author; ?></a>
-            </p>
+            <h2><a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h2>
+            <p class="lead">by <a href="index.php"><?php echo $post_author; ?></a></p>
             <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
             <hr>
             <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
@@ -46,11 +43,7 @@
 
             <hr>
 
-            <?php
-
-                    }
-
-                ?>
+            <?php } ?>
 
         </div>
 
