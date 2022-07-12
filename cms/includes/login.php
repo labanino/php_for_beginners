231a3b69f6f92d466a7ba37916a49b396bfa22cb
Alex Labanino <ghostrider6610@gmail.com>
2022-07-11 20:07:55 -0400

    Validating user query front-end

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -3,8 +3,37 @@
 <?php
 
     if(isset($_POST['login'])) {
-        echo $username = $_POST['username'];
-        echo $password = $_POST['password'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $username = mysqli_real_escape_string($connection, $username);
+        $password = mysqli_real_escape_string($connection, $password);
+
+        $query = "SELECT * FROM users WHERE user_username = '{$username}' ";
+        $select_user_query = mysqli_query($connection, $query);
+
+        // Check connection
+        if(!$select_user_query) {
+            die("CONNECTION FAILED: " . mysqli_error($connection));
+        }
+
+        while($row = mysqli_fetch_array($select_user_query)) {
+            $db_user_id = $row['user_id'];
+            $db_username = $row['user_username'];
+            $db_user_password = $row['user_password'];
+            $db_user_firstname = $row['user_firstname'];
+            $db_user_lastname = $row['user_lastname'];
+            $db_user_role = $row['user_role'];
+        }
+
+        if($username !== $db_username && $password !== $db_user_password ) {
+            header("Location: ../index.php ");
+        } else if($username == $db_username && $password == $db_user_password ) {
+            header("Location: ../admin");
+        } else {
+            header("Location: ../index.php ");
+        }
+
     }
 
 ?>
