32889bd23b1063ff8a6395bc6362d1c30faa0d87
Alex Labanino <ghostrider6610@gmail.com>
2022-07-12 14:49:00 -0400

    Validating user admin

--- cms/admin/includes/admin_header.php
+++ cms/admin/includes/admin_header.php
@@ -3,6 +3,16 @@
 <?php include '../includes/db.php'; ?>
 <?php include 'functions.php'; ?>
 
+<?php
+
+    if(isset($_SESSION['user_role'])) {
+        if($_SESSION['user_role'] !== 'admin') {
+            header('Location: ../index.php');
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- cms/includes/login.php
+++ cms/includes/login.php
@@ -27,16 +27,14 @@
             $db_user_role = $row['user_role'];
         }
 
-        if($username !== $db_username && $password !== $db_user_password ) {
-            header("Location: ../index.php ");
-        } else if($username == $db_username && $password == $db_user_password ) {
+        if($username == $db_username && $password == $db_user_password ) {
 
             $_SESSION['user_username'] = $db_username;
             $_SESSION['user_firstname'] = $db_user_firstname;
             $_SESSION['user_lastname'] = $db_user_lastname;
             $_SESSION['user_role'] = $db_user_role;
 
-            header("Location: ../admin");
+            header("Location: ../admin ");
         } else {
             header("Location: ../index.php ");
         }
