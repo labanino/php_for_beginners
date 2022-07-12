84acf972b6a5096ee4a80d5d1a5439a4a934eebd
Alex Labanino <ghostrider6610@gmail.com>
2022-07-12 14:03:36 -0400

    Validating user admin

--- cms/admin/index.php
+++ cms/admin/index.php
@@ -14,7 +14,7 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Admin Panel
-                            <small>Username</small>
+                            <small><?php echo $_SESSION['user_username']; ?></small>
                         </h1>
                     </div>
                 </div>
