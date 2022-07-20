2848428dccfb9ad388d3819a5c9700b35c3fe5d6
Alex Labanino <ghostrider6610@gmail.com>
2022-07-19 23:14:18 -0400

    Adding post edit link

--- cms/includes/navigation.php
+++ cms/includes/navigation.php
@@ -37,23 +37,34 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="./">CMS Front</a>
+                <a class="navbar-brand" href="./">CMS</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                 <?php
-                    $query = "SELECT * FROM categories";
-                    $select_all_categories_query = mysqli_query($connection,$query);
+                    // $query = "SELECT * FROM categories";
+                    // $select_all_categories_query = mysqli_query($connection,$query);
 
-                    while($row = mysqli_fetch_assoc($select_all_categories_query)) {
-                        $cat_title = $row['cat_title'];
-                        echo "<li><a href='#'>{$cat_title}</a></li>";
-                    }
+                    // while($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                    //     $cat_title = $row['cat_title'];
+                    //     echo "<li><a href='#'>{$cat_title}</a></li>";
+                    // }
                 ?>
                 <li>
                     <a href="./admin/">Admin</a>
-                </li>
+                </li>  
+                <?php
+                    // Without it you won't see the link and session won't start
+                    if (session_status() === PHP_SESSION_NONE) session_start();
+
+                    if(isset($_SESSION['user_role'])) {
+                        if(isset($_GET['p_id'])) {
+                            $the_post_id = $_GET['p_id'];
+                            echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
+                        }
+                    } 
+                ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
