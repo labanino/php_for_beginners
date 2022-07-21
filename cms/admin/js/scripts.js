cd620bd3a4cb1f39c719638c9098a6bb1e4b11be
Alex Labanino <ghostrider6610@gmail.com>
2022-07-21 18:36:02 -0400

    Adding bulk options  to Post

--- cms/admin/js/scripts.js
+++ cms/admin/js/scripts.js
@@ -4,3 +4,18 @@ $(document).ready(function() {
     height: 200
   });
 }); 
+
+// Select all checkboxes in "View All Posts"
+$(document).ready(function () {
+  $('#selectAllBoxes').click(function (event) {
+    if (this.checked) {
+      $('.checkBoxes').each(function () {
+        this.checked = true;
+      });
+    } else {
+      $('.checkBoxes').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+});
