c4cb487b864e566e90597e7805c94402250b7e8d
Alex Labanino <ghostrider6610@gmail.com>
2022-08-15 17:48:22 -0400

    Adding a loader to the admin pages

--- cms/admin/js/scripts.js
+++ cms/admin/js/scripts.js
@@ -18,4 +18,10 @@ $(document).ready(function () {
       });
     }
   });
+
+  var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+  $("body").prepend(div_box);
+  $('#load-screen').delay(200).fadeOut(200, function () {
+    $(this).remove();
+  })
 });
